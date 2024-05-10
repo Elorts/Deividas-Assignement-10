3afa2bb63a5ff57d5e7fe07327f798c54a0e426b
deividas strole <deiwis@yahoo.com>
2024-05-09 21:05:21 -0700

    Inserted URL in app.prop file

--- src/main/java/com/coderscampus/DeividasAssignement10/web/MealPlannerController.java
+++ src/main/java/com/coderscampus/DeividasAssignement10/web/MealPlannerController.java
@@ -2,6 +2,7 @@ package com.coderscampus.DeividasAssignement10.web;
 
 import com.coderscampus.DeividasAssignement10.dto.SpoonacularResponseDay;
 import com.coderscampus.DeividasAssignement10.dto.SpoonacularResponseWeek;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,17 @@ import java.net.URI;
 @RestController
 public class MealPlannerController {
 
+    @Value("${myapp.api.url}")
+    private String apiUrl;
+
     @GetMapping("mealplanner/week")
     public ResponseEntity<SpoonacularResponseWeek> getWeekMeals(String numCalories, String diet, String exclusions) {
 
 
         RestTemplate rt = new RestTemplate();
 
-        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+        //"https://api.spoonacular.com/mealplanner/generate"
+        URI uri = UriComponentsBuilder.fromHttpUrl(apiUrl)
                 .queryParam("timeFrame", "week")
                 .queryParam("apiKey", "52008d4850ec4abcbd7b3bfe88f99ffc")
                 .build()
@@ -37,7 +42,7 @@ public class MealPlannerController {
 
         RestTemplate rt = new RestTemplate();
 
-        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+        URI uri = UriComponentsBuilder.fromHttpUrl(apiUrl)
                 .queryParam("timeFrame", "day")
                 .queryParam("apiKey", "52008d4850ec4abcbd7b3bfe88f99ffc")
                 .build()
