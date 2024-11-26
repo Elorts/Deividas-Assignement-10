1e73eb9b225cd1602b0d8b394cc2ec0d99beda62
deividas strole <deiwis@yahoo.com>
2024-11-25 18:49:19 -0800

    style

--- src/main/java/com/coderscampus/DeividasAssignement10/web/MealPlannerController.java
+++ src/main/java/com/coderscampus/DeividasAssignement10/web/MealPlannerController.java
@@ -35,7 +35,6 @@ public class MealPlannerController {
                 .toUri();
 
         ResponseEntity<SpoonacularResponseWeek> response =  rt.getForEntity(uri, SpoonacularResponseWeek.class);
-
         return response;
     }
 
